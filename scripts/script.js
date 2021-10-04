5a3c53d60b210cf1119a350847297e97a1d48bb0
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2021-10-04 15:30:22 +0200

    Trying to get info from user

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
+// Toggle Light&Dark
 let body = document.querySelector('body');
 let toggle = document.querySelector('.toggle');
 let form = document.querySelector('.searchForm');
 
+// Links
+let city = document.querySelector('.location');
+let website = document.querySelector('.website');
+let company = document.querySelector('.company');
+let twitter = document.querySelector('.twitter');
+// Stats
+let repos = document.querySelector('.repos');
+let followers = document.querySelector('.following');
+let following = document.querySelector('.followers');
+
+// Toggle
 toggle.addEventListener('click', (e) => {
     if (body.classList.value === 'dark') {
         toggle.innerHTML =
@@ -16,3 +28,16 @@ toggle.addEventListener('click', (e) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
+
+// fetch(`https://api.github.com/users/octocat`)
+//     .then((response) => response.json())
+// .then((data) => {
+// repos.innerHTML = data.repos;
+// followers.innerHTML = data.followers;
+// following.innerHTML = data.following;
+// city.innerHTML = data.
+// website.innerHTML = data.
+// company.innerHTML = data.
+// twitter.innerHTML = data.
+//     data.resources;
+// });
